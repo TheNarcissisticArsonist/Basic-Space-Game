1c88b5a17818742075e30f06943f55861235e5c9
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-16 15:52:29 -0400

    Added spaceship object.

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-var rocket = document.getElementById("rocket");
+var sprite = document.getElementById("rocket");
+
+var spaceship = {
+  posX: 600,
+  posY: 300,
+  velX: 0,
+  velY: 0,
+  velM: 0,
+  aclX: 0,
+  aclY: 0,
+  aclM: 0
+};
